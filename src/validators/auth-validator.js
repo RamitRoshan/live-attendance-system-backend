ab1f5a90f4c8f60c8ab8ec5d0364e18673a35525
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-26 16:49:47 +0530

    added auth validation

--- src/validators/auth-validator.js
+++ src/validators/auth-validator.js
@@ -3,8 +3,10 @@ const { z } = require("zod");
 const signupSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  password: z.string().min(6),
+  password: z.string().min(6).max(125),
   role: z.enum(["teacher", "student"]),
 });
 
 module.exports = { signupSchema };
+
+
